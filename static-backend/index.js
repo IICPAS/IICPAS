6e276b1d9ea2f391b93440d46b0bd4c7691ae95e
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-09-02 19:44:38 +0530

    UPDATE:update in code

--- static-backend/index.js
+++ static-backend/index.js
@@ -35,7 +35,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // MongoDB setup
 let db;
