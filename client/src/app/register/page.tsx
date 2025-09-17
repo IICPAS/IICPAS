42afd3584b256796a750fb34ed3d5fd17434dd6e
Priyanshu <priyanshupatel405@gmail.com>
2025-09-17 13:57:02 +0530

    Feat:Login resister

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
-import StudentAuth from "../components/StudentAuth";
+import StudentRegister from "../components/StudentRegister";
 
 const page = () => {
   return (
     <div>
       <Header />
-      <StudentAuth />
+      <StudentRegister />
     </div>
   );
 };
