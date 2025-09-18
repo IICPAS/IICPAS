ec6bbac40b9da57075cc02e9f98f296acfed4236
Priyanshu <priyanshupatel405@gmail.com>
2025-09-18 14:44:34 +0530

    Feat: Whishlist button

--- client/src/app/components/CourseSection.tsx
+++ client/src/app/components/CourseSection.tsx
@@ -256,18 +256,13 @@ export default function CoursesSection() {
               >
                 <svg
                   className="w-5 h-5"
-                  fill="currentColor"
+                  fill="white"
                   stroke="currentColor"
                   strokeWidth="1"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
-                  <path
-                    d="M18 11h-2v-2h2v2zm0 4h-2v-2h2v2zm-4-4h-2v-2h2v2zm0 4h-2v-2h2v2zm-4-4H6v-2h2v2zm0 4H6v-2h2v2z"
-                    fill="white"
-                    opacity="0.8"
-                  />
                 </svg>
               </button>
             </div>
