cc7638361accd51c8c834c75ccf3dc2560e15fc9
sachin-iam <sachinguptaiam@gmail.com>
2025-09-25 19:53:32 +0530

    Fix missing contactInfoRoutes import in backend - comment out until component is created

--- backend/index.js
+++ backend/index.js
@@ -71,7 +71,7 @@ import teacherRoutes from "./routes/teacherRoutes.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
 
 //Contact Info Routes
-import contactInfoRoutes from "./routes/contactInfoRoutes.js";
+// import contactInfoRoutes from "./routes/contactInfoRoutes.js";
 
 //Contact Form Routes
 import contactFormRoutes from "./routes/contactFormRoutes.js";
@@ -285,7 +285,7 @@ io.on('connection', (socket) => {
 // Make io available globally for use in controllers
 global.io = io;
 //Contact Info Routes
-app.use("/api/contact-info", contactInfoRoutes);
+// app.use("/api/contact-info", contactInfoRoutes);
 
 //Contact Form Routes
 app.use("/api/contact-form", contactFormRoutes);
