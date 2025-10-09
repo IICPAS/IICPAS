e4249a5b5192293183d93a8136d9b054c284307b
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-10-09 19:31:11 +0530

    Update grid layout in BlogsPage for improved responsiveness. Adjusted column settings to enhance visual consistency across different screen sizes.

--- client/src/app/blogs/page.jsx
+++ client/src/app/blogs/page.jsx
@@ -338,7 +338,7 @@ export default function BlogsPage() {
                   <div
                     className={`${
                       viewMode === "grid"
-                        ? "grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2"
+                        ? "grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3"
                         : "space-y-4"
                     }`}
                   >
